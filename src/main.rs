a5045c84a5d204914b92ac61b3516a15dc1fe3fb
KarthikeyaGundumogula <karthikeya200226@gmail.com>
2026-02-25 13:40:30 +0530

    refactored and resolved path err

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,19 @@
 use std::{collections::HashMap, str::FromStr, sync::Arc};
+mod types;
+use types::{ApiError, ApiResponse, Job, JobState};
 
 use axum::{
     Json, Router,
     extract::{Path, State},
-    http::StatusCode,
-    response::IntoResponse,
     routing::{get, post},
 };
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use tokio::sync::Mutex;
 use uuid::{self, Uuid};
 
-#[derive(Serialize, Clone)]
-pub struct Job {
-    job_id: Uuid,
-    job_type: String,
-    payload: serde_json::Value,
-    state: JobState,
-    attempts: u32,
-}
+use crate::types::PostJob;
 
-#[derive(Serialize, Clone)]
-pub enum JobState {
-    Queued,
-    Running,
-    Succeeded,
-    Failed,
-    Dead,
-    Cancelled,
-}
-
-enum ApiResponse {
-    OK,
-    Created(String),
-    JobData(Job),
-}
-
-enum ApiError {
-    Failed,
-    NotFound,
-}
 pub struct AppState {
     jobs: HashMap<Uuid, Job>,
-}
-
-impl IntoResponse for ApiResponse {
-    fn into_response(self) -> axum::response::Response {
-        match self {
-            Self::OK => (StatusCode::OK).into_response(),
-            Self::Created(str) => (StatusCode::CREATED, Json(str)).into_response(),
-            Self::JobData(data) => (StatusCode::OK, Json(data)).into_response(),
-        }
-    }
-}
-
-impl IntoResponse for ApiError {
-    fn into_response(self) -> axum::response::Response {
-        match self {
-            Self::Failed => (StatusCode::BAD_REQUEST).into_response(),
-            Self::NotFound => (StatusCode::NOT_FOUND).into_response(),
-        }
-    }
 }
 
 #[tokio::main]
@@ -71,7 +23,7 @@ async fn main() {
     }));
 
     let app = Router::new()
-        .route("/get/:job_id", get(get_jobs))
+        .route("/get/{job_id}", get(get_jobs))
         .route("/post", post(post_job))
         .with_state(app_state);
 
@@ -103,11 +55,6 @@ async fn get_jobs(
     }
 }
 
-#[derive(Deserialize)]
-pub struct PostJob {
-    pub job_type: String,
-    pub payload: Value,
-}
 async fn post_job(
     State(app): State<Arc<Mutex<AppState>>>,
     Json(data): Json<PostJob>,
---
+++ src/types.rs
@@ -0,0 +1,59 @@
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use uuid::Uuid;
+
+#[derive(Deserialize)]
+pub struct PostJob {
+    pub job_type: String,
+    pub payload: Value,
+}
+
+#[derive(Serialize, Clone)]
+pub struct Job {
+    pub job_id: Uuid,
+    pub job_type: String,
+    pub payload: serde_json::Value,
+    pub state: JobState,
+    pub attempts: u32,
+}
+
+#[derive(Serialize, Clone)]
+pub enum JobState {
+    Queued,
+    Running,
+    Succeeded,
+    Failed,
+    Dead,
+    Cancelled,
+}
+
+pub enum ApiResponse {
+    OK,
+    Created(String),
+    JobData(Job),
+}
+
+pub enum ApiError {
+    Failed,
+    NotFound,
+}
+
+impl IntoResponse for ApiResponse {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            Self::OK => (StatusCode::OK).into_response(),
+            Self::Created(str) => (StatusCode::CREATED, Json(str)).into_response(),
+            Self::JobData(data) => (StatusCode::OK, Json(data)).into_response(),
+        }
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            Self::Failed => (StatusCode::BAD_REQUEST).into_response(),
+            Self::NotFound => (StatusCode::NOT_FOUND).into_response(),
+        }
+    }
+}
